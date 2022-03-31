f0b8da3f71f146d71009d396857ee3a11129e01b
Aldo Enriquez <aldo.enriquez@michelada.io>
2022-03-30 19:07:25 -0600

    Creacion del funcionamiento de cuentas

---
+++ db/migrate/20220330153622_create_accounts.rb
@@ -0,0 +1,11 @@
+class CreateAccounts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :accounts do |t|
+      t.string :name, null: false
+      t.string :client_name, null: false
+      t.string :operation_manager_name, null: false
+
+      t.timestamps
+    end
+  end
+end
